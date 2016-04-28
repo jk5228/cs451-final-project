7ff892f4b83f5cee609c7c10d3f0e0edcb924871
Jason Kim <jasonkim.0815@gmail.com>
2016-04-28 16:27:44 -0400

    implement book page and modal view; implement view container and scrolling infrastructure

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
+function focus(elem) {
+	$('.focus').removeClass('focus');
+	elem.addClass('focus');
+}
+
 $(document).ready(function() {
-	console.log('hello world');
+
+	// Activate modal
+	$('#pl1').click(function() {
+		$('#modal').removeClass('hidden');
+	});
+
+	// close modal
+	$('#close').click(function() {
+		$('#modal').addClass('hidden');
+	});
+
+	// Enter explore
+	$('#button').click(function() {
+		var scrollX = $('#page2').position().left;
+		console.log(scrollX);
+		// $('#view').scrollLeft($('#page2').position().left);
+		$('#view').css('overflow-x', 'scroll');
+		TweenLite.to($('#view'), 0.2, {scrollTo: {x: scrollX}, ease: Power1.easeOut});
+		focus($('#page2'));
+		$('#view').css('overflow-x', 'hidden');
+	});
 });
