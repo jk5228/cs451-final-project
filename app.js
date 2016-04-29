7c49ccc739c9a892e9422a660a7c0d001c506665
Jason Kim <jasonkim.0815@gmail.com>
2016-04-28 21:23:50 -0400

    bug with overflow preventing card from pushing out of page

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
 	// Make explore pages draggable
-	Draggable.create('.explore', {type: 'scrollTop', throwProps: true, edgeResistance: 0.7});
+	Draggable.create('.explore', {type: 'scrollTop', throwProps: true, edgeResistance: 0.8});
 
 	// Activate modal
 	$('#pl1').click(function() {
@@ -17,7 +17,12 @@ $(document).ready(function() {
 	$(document).on('click', '.breadcrumb', function(e) {
 		e.stopPropagation();
 		$(this).removeClass('breadcrumb');
-		var scrollX = $(this).position().left;
+		var scrollX;
+		if ($(this).hasClass('page')) {
+			scrollX = $(this).position().left;
+		} else {
+			scrollX = $('#page2').position().left;
+		}
 		$('#view').css('overflow-x', 'scroll');
 		TweenLite.to($('#view'), 0.4, {scrollTo: {x: scrollX}, ease: Power1.easeInOut});
 		$('#view').css('overflow-x', 'hidden');
@@ -31,6 +36,25 @@ $(document).ready(function() {
 		TweenLite.to($('#view'), 0.3, {scrollTo: {x: scrollX}, ease: Power1.easeInOut});
 		$('#view').css('overflow-x', 'hidden');
 		$('#modal').addClass('breadcrumb');
+	});
+
+	// Reject clicks on dummy cards
+	$('.card:not(#series)').click(function(e) {
+		e.stopPropagation();
+		$('#series').addClass('highlight');
+		window.setTimeout(function() { $('#series').removeClass('highlight'); }, 1000);
+	});
+
+	// Enter series page
+	$('#series').click(function(e) {
+		e.stopPropagation();
+		if ($(this).hasClass('breadcrumb')) return;
+		var scrollX = $('#page3').position().left;
+		console.log(scrollX);
+		$('#view').css('overflow-x', 'scroll');
+		TweenLite.to($('#view'), 0.3, {scrollTo: {x: '+='+scrollX}, ease: Power1.easeInOut});
+		$('#view').css('overflow-x', 'hidden');
+		$('#series').addClass('breadcrumb');
 	});
 
 });
